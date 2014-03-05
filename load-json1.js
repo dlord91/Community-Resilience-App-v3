2469274f6d07210d41dfcc6cf8344ae4d14be0cd
David Lordan <110326755@umail.ucc.ie>
2014-03-05 15:13:29 +0000

    Update load-json1.js

--- load-json1.js
+++ load-json1.js
@@ -6,15 +6,12 @@ $(document).ready(function(){
 			dataType: 'jsonp',
 			jsonp: 'jsoncallback',
 			timeout: 5000,
-			$str_json = trim($str, '"');
-$guests = json_decode($str_json);
-var_dump($guests);
 			success: function(data, status){
-				$.each(data, function(i,item){ 
+				data, function(i,item){ 
 					var landmark = '<li><a href="sms://+'+item.contact1+'?body=I%20NEED%20HELP%20at%20this%20location%20" title="SOS">SOS</a></li>';
 				
 					output.append(landmark);
-				});
+				};
 			},
 			error: function(){
 			   output.text('There was an error loading the data.');

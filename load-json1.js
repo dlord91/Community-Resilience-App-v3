c0b333610f67c74c629e3c97eb8a2c6f3b1f3e99
David Lordan <110326755@umail.ucc.ie>
2014-03-05 14:59:17 +0000

    Create load-json1.js

---
+++ load-json1.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+		var output = $('#output');
+	
+		$.ajax({
+			url: 'areaConnect.php',
+			dataType: 'jsonp',
+			jsonp: 'jsoncallback',
+			timeout: 5000,
+			success: function(data, status){
+				$.each(data, function(i,item){ 
+					var landmark = '<li><a href="sms://+'+item.name+'?body=I%20NEED%20HELP%20at%20this%20location%20" title="SOS">SOS</a></li>';
+				
+					output.append(landmark);
+				});
+			},
+			error: function(){
+			   output.text('There was an error loading the data.');
+			}
+		});
+
+});
+

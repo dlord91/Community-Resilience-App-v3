e4bfde7be62b4e5e54f5f2d8658101d3c1ace71c
David Lordan <110326755@umail.ucc.ie>
2014-03-05 15:29:25 +0000

    Update load-json2.js

--- load-json2.js
+++ load-json2.js
@@ -8,13 +8,18 @@ $(document).ready(function(){
 			timeout: 5000,
 			success: function(data, status){
 				$.each(data, function(i,item){ 
-					var landmark = '<li>'+item.name+'</li>'
-					+ '<li>'+item.phone+'</li>'
-					+ '<li>'+item.homeAddress+'</li>'
+					var landmark = '<label  for="name">Full Name</label>
+				              	<input name="name" id="name" type="text" value="'+item.name+'"/>'
+				              	+ '<label  for="number">Phone Number</label>
+				              	<input name="number" id="number" type="number" value="'+item.phone+'"/>'
+					+ '<label for="description">Address</label>
+							  	<textarea name="address" id="address" placeholder="Address" type="text">'+item.homeAddress+'</textarea>'
 					+ '<li>'+item.contact1+'</li>'
 					+ '<li>'+item.contact2+'</li>'
 					+ '<li>'+item.contact3+'</li>'
 					+ '<li>'+item.details+'</li>';
+					
+									
 				
 					output.append(landmark);
 				});

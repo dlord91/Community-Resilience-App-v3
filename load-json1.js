3b813a19586e0cbb866ff59f45174f271785875f
David Lordan <110326755@umail.ucc.ie>
2014-03-05 15:03:40 +0000

    Update load-json1.js

--- load-json1.js
+++ load-json1.js
@@ -2,13 +2,16 @@ $(document).ready(function(){
 		var output = $('#output');
 	
 		$.ajax({
-			url: 'areaConnect.php',
+			url: 'sosConnect.php',
 			dataType: 'jsonp',
 			jsonp: 'jsoncallback',
 			timeout: 5000,
+			$str_json = trim($str, '"');
+$guests = json_decode($str_json);
+var_dump($guests);
 			success: function(data, status){
 				$.each(data, function(i,item){ 
-					var landmark = '<li><a href="sms://+'+item.name+'?body=I%20NEED%20HELP%20at%20this%20location%20" title="SOS">SOS</a></li>';
+					var landmark = '<li><a href="sms://+'+item.contact1+'?body=I%20NEED%20HELP%20at%20this%20location%20" title="SOS">SOS</a></li>';
 				
 					output.append(landmark);
 				});
